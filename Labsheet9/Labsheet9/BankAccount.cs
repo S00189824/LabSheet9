81e942257f2e37c54ac323952aff2ab2fd621ae2
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-04-03 18:21:43 +0100

    finished

---
+++ Labsheet9/Labsheet9/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet9
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; set; }
+        public decimal OverDraftLimit { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(decimal amount)
+        {
+            decimal avaolableFunds = Balance + OverDraftLimit;
+            if(amount <= avaolableFunds)
+                Balance -= amount;
+
+        }
+    }
+}
---
+++ Labsheet9/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Labsheet9;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDeposit()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            decimal ExpectedBalance = 200m;
+
+            //Act
+            acc1.Deposit(200m);
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+
+
+        [TestMethod]
+        public void TestMultiDeposit()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            decimal ExpectedBalance = 200m;
+
+            //Act
+            acc1.Deposit(100m);
+            acc1.Deposit(60m);
+            acc1.Deposit(40m);
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+
+        [TestMethod]
+        public void TestNewAccountHasZerobalance()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            decimal ExpectedBalance = 0m;
+
+            //Act
+            
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+
+        [TestMethod]
+        public void TestWithDrawStufficientFunds()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            acc1.Deposit(200m);
+            decimal ExpectedBalance = 100m;
+
+            //Act
+            acc1.WithDraw(100m);
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+
+        [TestMethod]
+        public void Test_Insufficient_Funds()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            acc1.Deposit(100m);
+            decimal ExpectedBalance = 100m;
+
+            //Act
+            acc1.WithDraw(200m);
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+
+        [TestMethod]
+        public void Test_Insufficient_Funds_With_OverDraft()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            acc1.OverDraftLimit = 200m;
+            decimal ExpectedBalance = -100m;
+
+            //Act
+            acc1.WithDraw(100m);
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+
+        [TestMethod]
+        public void Test_Insufficient_FundsWith_OverDrafts()
+        {
+            //Arrange
+            BankAccount acc1 = new BankAccount();
+            acc1.Deposit(200m);
+            decimal ExpectedBalance = 0m;
+
+            //Act
+            acc1.WithDraw(300m);
+
+            //Assert
+            Assert.AreEqual(ExpectedBalance, acc1.Balance);
+        }
+    }
+}
